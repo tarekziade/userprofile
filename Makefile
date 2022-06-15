84865a5e598f05af6e8b88652a62ce214b0b95d2
Tarek Ziadé <tarek@ziade.org>
2022-06-15 10:10:31 +0200

    basic push

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 
 install:
 	python3 -m venv .
-	bin/pip install bottle
+	bin/pip install bottle elasticsearch
+
+run:
+	bin/python server.py
--- server.py
+++ server.py
@@ -1,21 +1,53 @@
 from collections import defaultdict
 import json
+import os
 from bottle import route, run, template, request, hook, response
+from elasticsearch import Elasticsearch
 
-user_data = defaultdict(list)
+HERE = os.path.dirname(__file__)
+INDEX = "search-countly"
 
-@hook('after_request')
+
+class ElasticDB:
+    def __init__(self):
+        self.db = Elasticsearch(
+            "http://localhost:9200", basic_auth=("elastic", "changeme")
+        )
+        with open(os.path.join(HERE, "mapping.json")) as f:
+            self.mapping = json.loads(f.read())
+        self.create_index()
+
+    def create_index(self):
+        if self.db.indices.exists(index=INDEX):
+            return
+        data = {"mappings": self.mapping}
+        self.db.indices.create(index=INDEX, body=data)
+
+    def push_event(self, event):
+        # XXX use bulk
+        print(self.db.index(index=INDEX, document=event))
+
+
+db = ElasticDB()
+
+
+@hook("after_request")
 def enable_cors():
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers["Access-Control-Allow-Origin"] = "*"
 
 
-@route('/i')
+@route("/i")
 def some_api():
     query = dict(request.query)
-    if 'device_id' in query and 'events' in query:
-        user_data[query['device_id']].extend(json.loads(query['events']))
-        print(user_data[query['device_id']])
+    if "device_id" in query and "events" in query:
+        events = json.loads(query["events"])
+        for event in events:
+            event["device_id"] = query["device_id"]
+            db.push_event(event)
     return {"result": "Success"}
 
 
-run(host='localhost', port=8282, )
+run(
+    host="localhost",
+    port=8282,
+)
